9b4a772fe165b6c24911b6be4e45dc80f61582df
Bruno Stangarlin <bruno214214@gmail.com>
2023-11-30 00:55:19 -0300

    Persistencia

--- Back/src/ProEventos.Domain/Palestrante.cs
+++ Back/src/ProEventos.Domain/Palestrante.cs
@@ -2,7 +2,7 @@
 {
     public class Palestrante
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Nome { get; set; }
         public string MiniCurriculo { get; set; }
         public string ImagemURL { get; set; }
